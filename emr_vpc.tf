e1f9b1debea988b2f7e91ea82a7c0eea780a8af7
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-08-31 11:31:49 -0700

    set up vpc with 3 subnets
    Created a vpc with 3 subnets and connected to an s3 bucket:
    - aws_vpc
    - aws_availability_zones
    - aws_subnet
    - aws_internet_gateway
    - aws_route_table
    - aws_route_table_association
    - aws_vpc_endpoint

---
+++ emr_vpc.tf
@@ -0,0 +1,96 @@
+# Author: Timothy Kornish
+# CreatedDate: 8/31/2025
+# Description: set up a VPC with the following:
+#                 - aws_vpc
+#                 - aws_availability_zones
+#                 - aws_subnet
+#                 - aws_internet_gateway
+#                 - aws_route_table
+#                 - aws_route_table_association
+#                 - aws_vpc_endpoint
+# follows youtube tutorial: https://www.youtube.com/watch?v=8bOgOvz6Tcg&list=PLkbxKqEYgUFSDTNZ0LoTLWfPNBd7D3-iZ&index=11
+
+# create aws vpc resource
+resource "aws_vpc" "emr_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "emr-vpc"
+  }
+}
+
+# create a data list of availability zones
+data "aws_availability_zones" "emr_vpc_available" {
+  state = "available"
+}
+
+data "aws_region" "current" {}
+
+output "current_aws_region" {
+  description = "The name of the current AWS region."
+  value       = data.aws_region.current.region
+}
+
+# create a public subnet
+resource "aws_subnet" "emr_public_subnet" {
+  for_each          = toset(data.aws_availability_zones.emr_vpc_available.names)
+  vpc_id            = aws_vpc.emr_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.emr_vpc.cidr_block, 8, index(data.aws_availability_zones.emr_vpc_available.names, each.value))
+  availability_zone = each.value
+  tags = {
+    Name = "public-subnet-${each.key}"
+  }
+}
+
+# create internet gateway
+resource "aws_internet_gateway" "emr_igw" {
+  vpc_id = aws_vpc.emr_vpc.id
+  tags = {
+    Name = "EMRInternetGateway"
+  }
+}
+
+# create a route table for the vpc IPs
+resource "aws_route_table" "emr_vpc_route_table" {
+  vpc_id = aws_vpc.emr_vpc.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "emr_public_subnet_association" {
+  for_each       = toset(data.aws_availability_zones.emr_vpc_available.names)
+  subnet_id      = aws_subnet.emr_public_subnet[each.value].id
+  route_table_id = aws_route_table.emr_vpc_route_table.id
+}
+
+# create a vpc gateway endpoint pointing towards the s3 bucket
+resource "aws_vpc_endpoint" "emr_vpc_s3_gateway_endpoint" {
+  vpc_id       = aws_vpc.emr_vpc.id
+  service_name = "com.amazonaws.${data.aws_region.current.region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = [
+    aws_route_table.emr_vpc_route_table.id
+  ]
+
+  # Add a policy to restrict access to specific S3 buckets
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Principal = "*"
+        Action   = "s3:*"
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.emr_bucket.id}",
+          "arn:aws:s3:::${aws_s3_bucket.emr_bucket.id}/*",
+        ]
+      },
+    ]
+  })
+
+  tags = {
+    Name = "emr-s3-gateway-endpoint"
+  }
+}

48d5710dd5b442a250548dd8d9ee09c907eae0ca
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-08-31 18:48:01 -0700

    removing extra ingress
    some clusters are starting up, but 5 are stalled in creating mode

--- emr_cluster.tf
+++ emr_cluster.tf
@@ -118,12 +118,12 @@ resource "aws_security_group" "emr_cluster_access" {
     cidr_blocks = ["0.0.0.0/0"] # Restrict to your specific IP or CIDR range in production
   }
 
-  ingress {
-    from_port   = 8443 # EMR console (if needed)
-    to_port     = 8443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Restrict to your specific IP or CIDR range in production
-  }
+  #ingress {
+  #  from_port   = 8443 # EMR console (if needed)
+  #  to_port     = 8443
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"] # Restrict to your specific IP or CIDR range in production
+  #}
 
   egress {
     from_port   = 0
@@ -150,13 +150,14 @@ resource "aws_security_group" "emr_cluster_access" {
 #  name        = "emr_cluster_service_access"
 #  description = "Allow inbound traffic"
 #  vpc_id      = aws_vpc.emr_vpc.id
-#
-#  #ingress {
+#  ingress {
 #  protocol        = "tcp"
-#  #  # This rule is specific to the EMR service's managed prefix list.
-#  #  # The actual IP ranges are maintained by AWS.
-#  #  prefix_list_ids = [aws_vpc_endpoint.emr_vpc_s3_gateway_endpoint.prefix_list_id] # Replace with the EMR service prefix list ID for your region
-#  #}
+#  from_port       = 9443
+#  to_port         = 9443
+#    # This rule is specific to the EMR service's managed prefix list.
+#    # The actual IP ranges are maintained by AWS.
+#    prefix_list_ids = [aws_vpc_endpoint.emr_vpc_s3_gateway_endpoint.prefix_list_id] # Replace with the EMR service prefix list ID for your region
+#  }
 #  ingress {
 #    from_port   = 22
 #    to_port     = 22
@@ -178,7 +179,6 @@ resource "aws_security_group" "emr_cluster_access" {
 #      ingress,
 #    ]
 #}
-#
 #  tags = {
 #    name = "emr_cluster_service_access"
 #  }

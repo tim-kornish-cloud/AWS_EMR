8671a5a5a7f43333b92a94916ee82e6aa7931fce
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-07 15:07:19 -0700

    creating IAM file for all IAM resources

---
+++ V1/emr_iam.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "emr_service_role" {
+  name = "EMR_Service_Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "elasticmapreduce.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "emr_ec2_role" {
+  name = "EMR_EC2_Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "emr_service_role_policy" {
+  role       = aws_iam_role.emr_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticMapReduceFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "emr_ec2_role_policy" {
+  role       = aws_iam_role.emr_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role"
+}

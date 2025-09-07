780e6637196d30e2c91fa52978e8f840bdc835e9
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-07 15:07:08 -0700

    moving iam resources to separate file, adding steps to cluster

--- V1/emr_cluster.tf
+++ V1/emr_cluster.tf
@@ -8,51 +8,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_role" "emr_service_role" {
-  name = "EMR_Service_Role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "elasticmapreduce.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "emr_service_role_policy" {
-  role       = aws_iam_role.emr_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticMapReduceFullAccess"
-}
-
 resource "aws_iam_instance_profile" "emr_ec2_instance_profile" {
   name = "EMR_EC2_Instance_Profile"
   role = aws_iam_role.emr_ec2_role.name
-}
-
-resource "aws_iam_role" "emr_ec2_role" {
-  name = "EMR_EC2_Role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "emr_ec2_role_policy" {
-  role       = aws_iam_role.emr_ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role"
 }
 
 resource "aws_key_pair" "emr_cluster_key" {
@@ -96,53 +54,25 @@ resource "aws_emr_cluster" "emr_spark_cluster" {
   service_role = aws_iam_role.emr_service_role.arn
 
   log_uri = "s3://${aws_s3_bucket.emr_bucket.id}/monthly_build/2023-09/logs/" # If you have an S3 bucket for logs
-}
 
-resource "aws_security_group" "emr_cluster_access" {
-  name        = "emr_cluster_access"
-  description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.emr_vpc.id
-
-  #ingress {
-  #  from_port       = 9443
-  #  to_port         = 9443
-  #  protocol        = "tcp"
-  #  # This rule is specific to the EMR service's managed prefix list.
-  #  # The actual IP ranges are maintained by AWS.
-  #  prefix_list_ids = [aws_vpc_endpoint.emr_vpc_s3_gateway_endpoint.prefix_list_id] # Replace with the EMR service prefix list ID for your region
-  #}
-
-  ingress {
-    from_port   = 22 # SSH access
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Restrict to your specific IP or CIDR range in production
+  #first step to move python script to the master node
+  step {
+    name              = "Copy Python Script from S3"
+    action_on_failure = "CONTINUE" # Or "TERMINATE_CLUSTER" based on your needs
+    hadoop_jar_step {
+      jar  = "command-runner.jar"
+      args = ["aws", "s3", "cp", "s3://${aws_s3_bucket.emr_bucket.id}/monthly_build/2023-09/main.py", "/home/hadoop/your_script.py"]
+    }
   }
-
-  #ingress {
-  #  from_port   = 8443 # EMR console (if needed)
-  #  to_port     = 8443
-  #  protocol    = "tcp"
-  #  cidr_blocks = ["0.0.0.0/0"] # Restrict to your specific IP or CIDR range in production
-  #}
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  depends_on = [aws_subnet.emr_public_subnet]
-
-  lifecycle {
-    ignore_changes = [
-      ingress,
-      egress,
-    ]
-  }
-
-  tags = {
-    name = "emr_cluster_access"
+  #second step execute the script on a slave node commanded by master node.
+  step {
+    name              = "Execute Python Script"
+    action_on_failure = "CONTINUE" # Or "TERMINATE_CLUSTER"
+    hadoop_jar_step {
+      jar  = "command-runner.jar"
+      args = ["python","/home/hadoop/main.py", "Food_Establishment_Inspection_Data_20250906.csv", "--output_s3_uri", "s3://${aws_s3_bucket.emr_bucket.id}/monthly_build/2023-09/output"]
+    }
   }
 }
+
+
